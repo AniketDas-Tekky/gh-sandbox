78b16eec92bc8c0b4f7d820b9d6f817c4e73c880
Aniket Das <adas@Tekky-Sentry-MBP.local>
2021-06-08 12:40:37 -0700

    Purposeful error and testing sentry integration

---
+++ src/onboarding.py
@@ -0,0 +1,21 @@
+
+import sys
+import sentry_sdk
+
+sentry_sdk.init(
+    "https://e03c8dc2af3d45bdb0106009c59d30d3@o815923.ingest.sentry.io/5807218",
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0
+)
+
+
+def run():
+    value = int(sys.argv[1]) / int(sys.argv[2])
+    print(f"Value is {value}")
+
+
+if __name__ == '__main__':
+    run()
