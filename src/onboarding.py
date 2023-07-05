56a5cd2cff3fc69e933b62253a96e1cb2e73be2e
Aniket Das <aniket.das@sentry.io>
2023-07-05 14:26:08 -0700

    Added new link

--- src/onboarding.py
+++ src/onboarding.py
@@ -3,12 +3,12 @@ import sys
 import sentry_sdk
 
 sentry_sdk.init(
-    "http://72f1ce49f18445279f6a165779543f0b@localhost:8000/10",
+    dsn="https://fc6c674a719249f4abc7c8499de2747d@o815923.ingest.sentry.io/4505478865551360",
+
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
-    release=os.environ.get("COMMIT_SHA")
+    traces_sample_rate=1.0
 )
 
 
