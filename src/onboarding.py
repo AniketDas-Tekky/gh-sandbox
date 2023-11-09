813fe6e0c5921b7b4e5f70bb434ab2d5c0831024
Aniket Das <adas@Anikets-MacBook-Pro.local>
2023-11-09 15:53:48 -0800

    ha

--- src/onboarding.py
+++ src/onboarding.py
@@ -2,21 +2,24 @@ import os
 import sys
 import sentry_sdk
 
-sentry_sdk.init(
-    dsn="https://fc6c674a719249f4abc7c8499de2747d@o815923.ingest.sentry.io/4505478865551360",
+import sentry_sdk
 
+sentry_sdk.init(
+    dsn="https://1c5d784f7ad85c0ce3684ae39f352bf8@o4506186993238016.ingest.sentry.io/4506198600450048",
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
     # We recommend adjusting this value in production.
-    traces_sample_rate=1.0
+    profiles_sample_rate=1.0,
 )
-
 
 def run():
     # Comment for commit
     # New Comment
     val = [1,2,3]
-    print(f"{val[4]}") 
+    print(f"{val[2]}") 
 
 
 if __name__ == '__main__':
